1fb4d802f2fec1837e88ce9923b68e17ca276aa1
Arseny <arseny.ukolov@iress.com.au>
2020-08-18 15:16:57 +1000

    about

--- src/About.js
+++ src/About.js
@@ -33,6 +33,11 @@ import inked6 from "./public/Photos/about/inked/6.jpg";
 import misfit1 from "./public/Photos/about/misfit/1.jpg";
 import misfit2 from "./public/Photos/about/misfit/2.jpg";
 import misfit3 from "./public/Photos/about/misfit/3.jpg";
+import misfit4 from "./public/Photos/about/misfit/4.jpg";
+
+import online1 from "./public/Photos/about/online/1.jpg";
+import online2 from "./public/Photos/about/online/2.jpg";
+import online3 from "./public/Photos/about/online/3.jpg";
 
 class About extends React.Component {
   constructor(props) {
@@ -44,7 +49,7 @@ class About extends React.Component {
     if (this.pc) {
       return (
         <GridList className="gridlist" cols={1}>
-          <Box display="flex" flexWrap="wrap" justifyContent="center">
+          <Box display="flex" flexWrap="wrap" justifyContent="stretch">
             <Box display="flex">
               <Box key="1" justifyContent="center" p={1}>
                 <img src={avatar} alt="avatar" height="600" />
@@ -90,30 +95,32 @@ class About extends React.Component {
                 </Box>
               </Box>
             </Box>
-            <br />
-            <br />
             <Box display="flex">
               <Grid className="gridlist" container>
                 <Grid item xs={6}>
-                  <Typography align="center" variant="h5">
-                    {t("About.Interviews")}
+                  <Typography align="left" variant="h5">
+                    <br />
+                    <br />
+                    INTERVIEW
                   </Typography>
                   <Typography variant="subtitle1" align="left">
                     <br />
                     <br />
-                    {t("Interviews.title1")}
+                    28.04.2020 Echo Chambers
                     <br />
-                    {t("Interviews.text1")}
-                    <br />
-                    <br />
-                    {t("Interviews.title2")}
-                    <br />
-                    {t("Interviews.text2")}
+                    View: Adjusting your work to others leads to a dead-end
                     <br />
                     <br />
-                    {t("Interviews.title3")}
+                    06.01.2020 SBS Russia
                     <br />
-                    {t("Interviews.text3")}
+                    Melbourne photographer Valentin Zhmodikov talks about art,
+                    tattoos and the shooting of Boris Grebenshchikov
+                    <br />
+                    <br />
+                    XX.XX.2020 Savage Thrills
+                    <br />
+                    Spending time with Russian Portrait Photographer - Valentin
+                    Zhmodikov
                   </Typography>
                 </Grid>
                 <Grid item xs={6}>
@@ -127,48 +134,45 @@ class About extends React.Component {
                   <Typography align="center" variant="h5">
                     <br />
                     <br />
-                    {t("About.SelectedPublications1")}
+                    SELECTED PUBLICATIONS. TATTOO
                   </Typography>
-                </Grid>
-                <Grid item xs={4}>
+                  <Typography align="center" variant="subtitle1">
+                    TTISM, ISSUE 4, 2020 & INKED AUSTRALIA, ISSUE 44, 2017
+                  </Typography>
                   <img src={inked1} alt="inked1" width="600" />
                 </Grid>
                 <Grid item xs={4}>
                   <img src={inked2} alt="inked2" width="600" />
-                </Grid>
-                <Grid item xs={4}>
                   <img src={inked3} alt="inked3" width="600" />
                 </Grid>
                 <Grid item xs={4}>
                   <img src={inked4} alt="inked4" width="600" />
-                </Grid>
-                <Grid item xs={4}>
                   <img src={inked5} alt="inked5" width="600" />
-                </Grid>
-                <Grid item xs={4}>
-                  <img src={inked6} alt="inked6" width="600" />
                 </Grid>
               </Grid>
             </Box>
 
             <Box display="flex">
               <Grid className="gridlist" container>
-                <Grid item xs={4}>
-                  <Typography align="center" variant="h5">
-                    <br />
-                    <br />
-                    {t("About.SelectedPublications2")}
-                  </Typography>
-                </Grid>
                 <Grid item xs={4}>
                   <img src={misfit1} alt="misfit1" width="600" />
                 </Grid>
                 <Grid item xs={4}>
-                  <img src={misfit2} alt="misfit2" width="600" />
+                  <img src={misfit2} alt="misfit2" width="600" height="422" />
+                  <img src={misfit3} alt="misfit3" width="600" height="422" />
                 </Grid>
                 <Grid item xs={4}>
-                  <img src={misfit3} alt="misfit3" width="600" />
+                  <Typography align="center" variant="h5">
+                    <br />
+                    <br />
+                    SELECTED PUBLICATIONS. LIFESTYLE
+                  </Typography>
+                  <Typography align="center" variant="subtitle1">
+                    MSIFIT, AUGUST, 2016 & BROAD MAGAZINE, ISSUE 3, 2017
+                  </Typography>
+                  <img src={misfit4} alt="misfit4" width="600" />
                 </Grid>
+                <Grid item xs={4}></Grid>
               </Grid>
             </Box>
 
@@ -178,8 +182,28 @@ class About extends React.Component {
                   <Typography align="center" variant="h5">
                     <br />
                     <br />
-                    {t("About.SelectedPublications3")}
+                    SELECTED PUBLICATIONS. ONLINE
                   </Typography>
+                  <Typography align="left" variant="subtitle1">
+                    <br />
+                    ART: NAKID, PAINTINGS ALIVE BY VALENTIN ZHMODIKOV
+                    <br />
+                    <br />
+                    LIFESTYLE: REBEL & CO MAGAZINE, DIVE THRU BALTISOUL, HAPPY
+                    DAYS
+                    <br />
+                    <br />
+                    MUSIC: SAVAGE THRILLS RUFUS DU SOL MELBOURNE LIVE LIARS,
+                    MELBOURNE LIVE GOGOL BARDELLO, MELBOURNE LIVE AND MANY
+                    OTHERS
+                  </Typography>
+                </Grid>
+                <Grid item xs={4}>
+                  <img src={online1} alt="online1" width="600" />
+                  <img src={online2} alt="online2" width="600" />
+                </Grid>
+                <Grid item xs={4}>
+                  <img src={online3} alt="online3" width="600" />
                 </Grid>
               </Grid>
             </Box>
